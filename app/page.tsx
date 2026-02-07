116e5555805463f9a0427a30c4d8dceedc7359b8
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2026-02-07 11:34:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ import Beams from "@/components/Beams";
 import Hero from "@/components/LandingPage/Hero";
 import Features from "@/components/LandingPage/Features";
 import Integration from "@/components/LandingPage/Integration";
+import CTA from "@/components/LandingPage/CTA";
 import Footer from "@/components/LandingPage/Footer";
+import { isMobile } from "react-device-detect";
 
 export const dynamic = "force-static";
 
@@ -16,52 +18,56 @@ export default function Home() {
 
   return (
     <>
-      {/* <div className="min-h-screen relative">
-        <div className="w-full h-screen relative">
-          <Beams
-            beamWidth={3}
-            beamHeight={16}
-            beamNumber={12}
-            lightColor="#ffffff"
-            speed={4}
-            noiseIntensity={1.75}
-            scale={0.3}
-            rotation={30}
-          />
+      {isMobile ? (
+        <div className="min-h-screen relative">
+          <div className="w-full h-screen relative">
+            <Beams
+              beamWidth={3}
+              beamHeight={16}
+              beamNumber={12}
+              lightColor="#ffffff"
+              speed={4}
+              noiseIntensity={1.75}
+              scale={0.3}
+              rotation={30}
+            />
 
-          <div className="absolute inset-0">
-            <div className="flex flex-col items-center justify-center h-full text-center px-4">
-              <Image
-                src={process.env.NEXT_PUBLIC_APP_LOGO!}
-                alt="DocsPlus | Office Document Management System"
-                width={300}
-                height={300}
-                loading="eager"
-                priority
-                className="mb-8 rounded-full object-cover h-48 w-48 md:h-72 md:w-72"
-              />
-              <h1 className="text-4xl md:text-7xl font-bold text-white mb-4">
-                Welcome to {process.env.NEXT_PUBLIC_COMPANY_NAME}
-              </h1>
-              <p className="text-lg md:text-2xl text-white mb-8 max-w-2xl">
-                Generate and manage salary slips with ease. Streamline your
-                payroll process today!
-              </p>
-              <Button
-                size="lg"
-                variant="outline"
-                className="hover:cursor-pointer"
-                onClick={() => router.push("/dashboard")}
-              >
-                Get Started
-              </Button>
+            <div className="absolute inset-0">
+              <div className="flex flex-col items-center justify-center h-full text-center px-4">
+                <Image
+                  src={process.env.NEXT_PUBLIC_APP_LOGO!}
+                  alt="DocsPlus | Office Document Management System"
+                  width={300}
+                  height={300}
+                  loading="eager"
+                  priority
+                  className="mb-8 rounded-full object-cover h-48 w-48 md:h-72 md:w-72"
+                />
+                <h1 className="text-4xl md:text-7xl font-bold text-white mb-4">
+                  Welcome to {process.env.NEXT_PUBLIC_COMPANY_NAME}
+                </h1>
+                <p className="text-lg md:text-2xl text-white mb-8 max-w-2xl">
+                  Generate and manage salary slips with ease. Streamline your
+                  payroll process today!
+                </p>
+                <Button
+                  size="lg"
+                  variant="outline"
+                  className="hover:cursor-pointer"
+                  onClick={() => router.push("/dashboard")}
+                >
+                  Get Started
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div> */}
-      <Hero />
+      ) : (
+        <Hero />
+      )}
       <Features />
-      <Integration />
+      {!isMobile && <Integration />}
+      <CTA />
       <Footer />
     </>
   );
